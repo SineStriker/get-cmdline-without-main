50489b04554cccfc60f8ddaf5df9f9357172c014
Sine Striker <trueful@163.com>
2023-10-22 22:58:06 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# get-cmdline-without-main
-Get Command Line Arguments Independent From Entry
+# Get CommandLine
+
+Get Command Line Arguments Independent From Entry.
+
+## Test
+
+```c++
+std::vector<std::string> commandLineArguments();
+
+int main(int /* argc */, char * /* argv */[]) {
+  auto args = commandLineArguments();
+  for (int i = 0; i < args.size(); ++i) {
+    u8printf("%-4d %s\n", i, args[i].data());
+  }
+  return 0;
+}
+```
+
+```sh
+> getcmdline.exe a 33 我 ◆ 😀
+0    getcmdline.exe
+1    a
+2    33
+3    我
+4    ◆
+5    😀
+```
+
+The command line arguments are returned as UTF-8 encoded `std::string` list on all platforms including Windows.
--- main.cpp
+++ main.cpp
@@ -1,35 +1,52 @@
-#include <fstream>
-#include <iostream>
-#include <sstream>
+#include <cstdarg>
+#include <string>
 #include <vector>
 
 #ifdef _WIN32
-#include <Windows.h>
-
-using TChar = wchar_t;
-using TString = std::wstring;
-#else
-#ifdef __APPLE__
+#include <windows.h>
+#elif defined(__APPLE__)
 #include <crt_externs.h>
-#endif
-using TChar = char;
-using TString = std::string;
+#else
+#include <fstream>
 #endif
 
-static std::vector<TString> get_command_line() {
-  std::vector<TString> res;
+#ifdef _WIN32
+std::string wideToUtf8(const std::wstring &utf16Str) {
+  if (utf16Str.empty()) {
+    return {};
+  }
+  const auto utf16Length = static_cast<int>(utf16Str.size());
+  if (utf16Length >= (std::numeric_limits<int>::max)()) {
+    return {};
+  }
+  const int utf8Length =
+      ::WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, utf16Str.data(),
+                            utf16Length, nullptr, 0, nullptr, nullptr);
+  if (utf8Length <= 0) {
+    return {};
+  }
+  std::string utf8Str;
+  utf8Str.resize(utf8Length);
+  ::WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, utf16Str.data(),
+                        utf16Length, &utf8Str[0], utf8Length, nullptr, nullptr);
+  return utf8Str;
+}
+#endif
+
+std::vector<std::string> commandLineArguments() {
+  std::vector<std::string> res;
 #ifdef _WIN32
   int argc;
-  auto argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+  auto argvW = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
   if (argvW == nullptr)
     return {};
   res.reserve(argc);
   for (int i = 0; i != argc; ++i) {
-    res.push_back(argvW[i]);
+    res.push_back(wideToUtf8(argvW[i]));
   }
-  LocalFree(argvW);
+  ::LocalFree(argvW);
 #elif defined(__APPLE__)
-  char **argv = *(_NSGetArgv());
+  auto argv = *(_NSGetArgv());
   for (int i = 0; argv[i] != nullptr; ++i) {
     res.push_back(argv[i]);
   }
@@ -38,31 +55,35 @@ static std::vector<TString> get_command_line() {
   if (file.fail())
     return {};
   std::string s;
-  char c;
-  while (file.get(c)) {
-    if (c == '\0') {
-      if (!s.empty()) {
-        res.push_back(s);
-        s.clear();
-      }
-    } else {
-      s.push_back(c);
-    }
+  while (std::getline(file, s, '\0')) {
+    res.push_back(s);
   }
+  file.close();
 #endif
   return res;
 }
 
-int main() {
-  auto args = get_command_line();
+int u8printf(const char *fmt, ...) {
 #ifdef _WIN32
-  for (int i = 0; i < args.size(); ++i) {
-    wprintf(L"%-4d %s\n", i, args[i].data());
-  }
-#else
-  for (int i = 0; i < args.size(); ++i) {
-    printf("%-4d %s\n", i, args[i].data());
-  }
+  auto cp = ::GetConsoleOutputCP();
+  ::SetConsoleOutputCP(CP_UTF8);
 #endif
+
+  va_list args;
+  va_start(args, fmt);
+  int res = vprintf(fmt, args);
+  va_end(args);
+
+#ifdef _WIN32
+  ::SetConsoleOutputCP(cp);
+#endif
+  return res;
+}
+
+int main(int /* argc */, char * /* argv */[]) {
+  auto args = commandLineArguments();
+  for (int i = 0; i < args.size(); ++i) {
+    u8printf("%-4d %s\n", i, args[i].data());
+  }
   return 0;
 }
